baddc7ccbbd2572ddc42dbdbd83f3e698b28f46c
Hachi8888 <h.nana.420@gmail.com>
2022-07-02 23:21:52 +0900

    add composable function

--- app/src/main/java/com/websarva/wings/android/countupapp/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/countupapp/MainActivity.kt
@@ -4,12 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           Text(text = "Hello World")
+            HelloWorld()
         }
     }
  }
+
+/**
+ * Textに「Hello World!!」と表示するComposable関数
+ */
+@Composable
+fun HelloWorld() {
+    Text(text = "Hello World!!")
+}

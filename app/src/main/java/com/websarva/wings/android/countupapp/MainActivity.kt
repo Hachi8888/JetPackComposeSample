76d70de7f9215ae5ff031889d7a54b6337ceaa9e
Hachi8888 <h.nana.420@gmail.com>
2022-07-03 08:23:33 +0900

    add composable countUp function

--- app/src/main/java/com/websarva/wings/android/countupapp/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/countupapp/MainActivity.kt
@@ -3,14 +3,16 @@ package com.websarva.wings.android.countupapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HelloWorld()
+            CountUp()
         }
     }
  }
@@ -21,4 +23,35 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun HelloWorld() {
     Text(text = "Hello World!!")
+}
+
+/**
+ * カウントアップボタンと現在のカウント回数を
+ * 表示するComposable関数
+ *
+ * カウントアップボタンが押下されるたびにカウントの回数を
+ * 1ずつ増やし、表示を更新する。
+ * 内部的にMutableState<Int>を用いてカウントの回数を
+ * 保持している。
+ */
+@Composable
+fun CountUp() {
+    var count: Int by remember {
+    mutableStateOf(0)
+    }
+
+    Column {
+        Text(
+            text = "$count"
+        )
+        Button(
+            onClick = {
+            count += 1
+            }
+        ) {
+            Text(
+                text = "Count Up!"
+            )
+        }
+    }
 }
